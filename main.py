0c8a93b9144af1931e88fc8561cdaa71507ac89e
deus <104161384+DeusWeb@users.noreply.github.com>
2022-05-12 22:39:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 import os
 import time
+import requests
 import urllib.request
 from getpass import getpass
 from colorama import Fore, Back, Style
 import getpass 
 
-
-passwd = urllib.request.urlopen("pastebin url here")
+#modify the link by putting the connection keys
+passwd = urllib.request.urlopen("https://pastebin.com/raw/p5XBSN31")
 passutf = passwd.read().decode("utf8")
 passutf = passutf.split("\r\n")
 
+#edit variable VERSION for uptdate
+VERSION = "1.1"
+#edit link version (raw)
+version = requests.get("https://pastebin.com/raw/ScLewjnu").content.decode('utf-8')
 
-banner = ("""
-
-
-| Sample-Login | 1.0
+banner = (f"""
+| Sample-Login | {VERSION}
 |══════════════════════════════════════════════
 | By DEUS-WEB#9999
 | Discord : discord.gg/deusweb
 |══════════════════════════════════════════════
-| Enter your key lisence.
+| Welcome to Sample-Login.
 """)
 
+
 def menu():
-    print(Fore.GREEN + "Succesfully login ! thanks you")
+    print(Fore.YELLOW + "> " + "Succesfully login ! thanks you")
 
 def login():
+  if version != VERSION:
+    
+    print(Fore.YELLOW + "> " + Fore.RED + "Update available")
+  else:
     os.system('cls' if os.name == 'nt' else 'clear')
     print(Fore.BLUE + banner)
-    pwd = getpass.getpass(prompt = Fore.BLUE + "Key: ")
+    pwd = getpass.getpass(prompt = Fore.YELLOW + "> " + Fore.BLUE + "Key: ")
     if pwd in passutf: 
        os.system('cls' if os.name == 'nt' else 'clear')
        print(Fore.GREEN + "Succes key!")
